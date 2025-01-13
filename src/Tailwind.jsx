ae00c9405be268dfe714445f614dee708030f46c
Rokibul Alom <itismehey975@gmail.com>
2025-01-13 21:14:08 +0600

    tailwind and daisy ui insatallded and setup

---
+++ src/Tailwind.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tailwind = () => {
+    return (
+        <div>
+            <h1 className='text-red-500 text-5xl'>checking is tailwind workingn or not</h1>
+        </div>
+    );
+};
+
+export default Tailwind;
